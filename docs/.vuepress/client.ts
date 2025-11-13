7d0db97a1d268675ce915630c6837001ac5028a4
Jursin <jursin@126.com>
2025-11-13 22:39:20 +0800

    优化样式

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -2,7 +2,6 @@ import { defineClientConfig } from 'vuepress/client'
 import Home from './theme/components/Home.vue'
 import Detail from './theme/components/Detail.vue'
 
-import './theme/styles/index.css'
 import './theme/styles/custom.css'
 
 export default defineClientConfig({
